6a63544287f4c66ccfa92c0c7beb2f7b5b732829
Alex Lewanski <alexlewanski@Mac.home.local>
2026-09-17 15:11:07 -0400

    updates to tree process script: multiple tree files and mutiple samp size/tree file

--- code/scripts/slim_run_processing_scripts/run_tree_process_script.sh
+++ code/scripts/slim_run_processing_scripts/run_tree_process_script.sh
@@ -2,31 +2,66 @@
 
 #make sure to have slim_work conda enviro activated
 
-
+basename_file="/Users/alexlewanski/Documents/michigan_state/research/location_imputation/simulation_output/basename_file.txt"
 raw_tree_path="/Users/alexlewanski/Documents/michigan_state/research/location_imputation/simulation_output/postburnin_sims"
-#raw_tree_file="POSTBURNIN_BURNINREP1_RUNTIME10000_DISP1.80_CC0.15_K3_Kreps15.trees"
-#raw_tree_file="POSTBURNIN_BURNINREP1_RUNTIME10000_DISP1.25_CC0.15_K3_Kreps16.trees"
-#raw_tree_file="POSTBURNIN_BURNINREP1_RUNTIME8000_DISP1.25_CC0.11_K3_Kreps16.trees"
-#raw_tree_file="POSTBURNIN_BURNINREP1_RUNTIME8000_DISP1.50_CC0.13_K2_Kreps16.trees"
-#raw_tree_file="POSTBURNIN_BURNINREP1_RUNTIME8000_DISP1.50_CC0.15_K2_Kreps16.trees"
-raw_tree_file="POSTBURNIN_BURNINREP1_RUNTIME9000_DISP2.0_CC0.17_K1_Kreps16.trees"
+output_dir=/Users/alexlewanski/Documents/michigan_state/research/location_imputation/simulation_output/processed_treeseq
 
-top_level_dir=/Users/alexlewanski/Documents/michigan_state/research/location_imputation/simulation_output/processed_treeseq
+sample_size=(50 150)
 
-output_file_name=$(basename ${raw_tree_file} .trees)
-mkdir ${top_level_dir}/${output_file_name}
+while IFS= read -r FILE_NAME; do
+    raw_tree_file=${FILE_NAME}.trees
 
-python3 process_tree_file_commandline.py \
---tree_sep 30 \
---sample_size 250 \
---ts_upload_path ${raw_tree_path}/ \
---ts_name ${raw_tree_file} \
---output_file_path ${top_level_dir}/${output_file_name}/ \
---output_tree_path ${top_level_dir}/${output_file_name}/ \
---output_prefix $output_file_name \
---recomb_rate 1e-8 \
---ne 500 \
---seed 342342
+    if [ ! -f "${raw_tree_path}/$raw_tree_file" ]; then
+        echo "The tree file does not exist!"
+        exit 1
+    else
+        echo "Tree file exists! Proceeding with processing."
+    fi
+
+    for SAMP_SIZE in "${sample_size[@]}"; do
+
+        #echo $raw_tree_file
+
+        output_file_name=${FILE_NAME}_sampsize${SAMP_SIZE}
+        #output_file_name=$(basename ${raw_tree_file} .trees)_sampsize${SAMP_SIZE}
+        mkdir ${output_dir}/${output_file_name}
+
+        python3 process_tree_file_commandline_alt.py \
+        --tree_sep 50 \
+        --sample_size $SAMP_SIZE \
+        --ts_upload_path ${raw_tree_path}/ \
+        --ts_name ${raw_tree_file} \
+        --output_file_path ${output_dir}/${output_file_name}/ \
+        --output_tree_path ${output_dir}/${output_file_name}/ \
+        --output_prefix $output_file_name \
+        --recomb_rate 1e-8 \
+        --ne 500 \
+        --seed $RANDOM
+    done
+done < ${basename_file}
+
+
+
+
+#for FILE in core_names; do
+#    for SAMP_SIZE in "${sample_size[@]}"; do
+
+#        output_file_name=$(basename ${raw_tree_file} .trees)_sampsize${SAMP_SIZE}
+#        mkdir ${top_level_dir}/${output_file_name}
+
+#        python3 process_tree_file_commandline_alt.py \
+#        --tree_sep 50 \
+#        --sample_size 150 \
+#        --ts_upload_path ${raw_tree_path}/ \
+#        --ts_name ${raw_tree_file} \
+#        --output_file_path ${top_level_dir}/${output_file_name}/ \
+#        --output_tree_path ${top_level_dir}/${output_file_name}/ \
+#        --output_prefix $output_file_name \
+#        --recomb_rate 1e-8 \
+#        --ne 500 \
+#        --seed 342322
+#    done
+#done
 
 #python3 test.py \
 #-n 3 \
